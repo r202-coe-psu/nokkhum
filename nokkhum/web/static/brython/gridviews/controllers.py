e31f09f750274841834bd02b0dd6758c117865ef
Somboon Changkhom <mypspgood@gmail.com>
2020-10-20 16:28:40 +0700

    drag camera from display to another display

--- nokkhum/web/static/brython/gridviews/controllers.py
+++ nokkhum/web/static/brython/gridviews/controllers.py
@@ -8,12 +8,16 @@ class GridViewController:
     def mouseover(self, ev):
         ev.target.style.cursor = "pointer"
 
+    def dragstart_img(self, ev):
+        data = ev.target.id[4:]
+        ev.dataTransfer.setData("text", data)
+        ev.dataTransfer.effectAllowed = "move"
+
     def dragstart(self, ev):
         ev.dataTransfer.setData("text", ev.target.id)
         ev.dataTransfer.effectAllowed = "move"
 
     def dragover(self, ev):
-
         ev.dataTransfer.dropEffect = "move"
         ev.preventDefault()
 
@@ -27,19 +31,28 @@ class GridViewController:
         camera = document[src]
         camera_id, url = src.split("-")
         display = document[ev.target.id]
+        try:
+            document[f"img-{src}"].unbind("dragstart")
+            span = html.SPAN("Drag camera to here.", Class="ui large text")
+            document[f"img-{src}"].parent <= html.P(
+                span, style={"padding-top": "10rem"}
+            )
+            del document[f"img-{src}"]
 
-        img = html.IMG(src=url, width="100%")
+        except Exception as e:
+            pass
+        img = html.IMG(id=f"img-{src}", src=url, width="100%")
         display.clear()
         display <= img
         # # set the new coordinates of the dragged object
         # display.style.left = "{}px".format(ev.x - m0[0])
         # display.style.top = "{}px".format(ev.y - m0[1])
         # # don't drag the object any more
-        display.draggable = False
-
+        img.draggable = True
+        img.bind("dragstart", self.dragstart_img)
         # # remove the callback function
-        display.unbind("mouseover")
-        display.style.cursor = "auto"
+        # display.unbind("mouseover")
+        # display.style.cursor = "auto"
         ev.preventDefault()
 
     def start(self):
