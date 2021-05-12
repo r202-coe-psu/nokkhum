ae0124116cb84f9a3f21ea051727343c2152d2ff
Thanathip Limna <boatkrap@gmail.com>
2021-05-12 12:26:36 +0700

    remove flask allow

--- nokkhum/web/acl.py
+++ nokkhum/web/acl.py
@@ -1,95 +1,58 @@
-from flask import redirect, url_for, request, session
-from flask_login import LoginManager, UserMixin, current_user, login_url
-from flask_allows import Allows
+from flask import redirect, url_for, request, redirect
+from flask_login import LoginManager, current_user, login_url
+from flask_principal import (Principal,
+                             Permission,
+                             UserNeed,
+                             RoleNeed,
+                             identity_loaded
+                             )
 
-from .. import models
-
-allows = Allows(identity_loader=lambda: current_user)
-
-
-def is_admin(ident, request):
-    return "admin" in ident.roles
-
-
-def is_developer(ident, request):
-    return "developer" in ident.roles
+from . import models
 
 
-def is_staff(ident, request):
-    return "staff" in ident.roles
+login_manager = LoginManager()
+principals = Principal()
 
 
-def is_lecturer(ident, request):
-    return "lecturer" in ident.roles
-
-
-def is_teaching_assistant(ident, request):
-    class_id = request.view_args.get("class_id", None)
-    solution_id = request.view_args.get("solution_id", None)
-
-    if class_id:
-        try:
-            class_ = models.Class.objects.get(
-                id=class_id, teaching_assistants__user=ident._get_current_object()
-            )
-            if class_:
-                return True
-        except Exception as e:
-            return False
-    elif solution_id:
-        try:
-            solution = models.Solution.objects.get(id=solution_id)
-
-            if solution.enrolled_class.is_teaching_assistant(
-                ident._get_current_object()
-            ):
-                return True
-        except Exception as e:
-            return False
-
-    return False
-
-
-def is_solution_owner(ident, request):
-    try:
-        solution = models.Solution.objects.get(
-            id=request.view_args.get("solution_id"), owner=ident._get_current_object()
-        )
-        if solution:
-            return True
-    except Exception as e:
-        return False
-
-    return False
-
-
-def is_class_owner(ident, request):
-
-    try:
-        class_ = models.Class.objects.get(
-            id=request.view_args.get("class_id"), owner=ident._get_current_object()
-        )
-        if class_:
-            return True
-    except Exception as e:
-        return False
-
-    return False
+# permissions
+admin_permission = Permission(RoleNeed('admin'))
+# lecturer_permission = Permission(RoleNeed('lecturer'))
 
 
 def init_acl(app):
     # initial login manager
-    login_manager = LoginManager(app)
 
-    @login_manager.user_loader
-    def load_user(user_id):
-        user = models.User.objects.with_id(user_id)
-        return user
+    login_manager.init_app(app)
+    principals.init_app(app)
 
-    @login_manager.unauthorized_handler
-    def unauthorized_callback():
-        if request.method == "GET":
-            response = redirect(login_url("accounts.login", request.url))
-            return response
 
-        return redirect(url_for("accounts.login"))
+@login_manager.user_loader
+def load_user(user_id):
+    user = models.User.objects.with_id(user_id)
+    return user
+
+
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    if request.method == 'GET':
+        response = redirect(
+                login_url('accounts.login', request.url))
+        return response
+
+    return redirect(url_for('accounts.login'))
+
+
+@identity_loaded.connect
+def on_identity_loaded(sender, identity):
+    # Set the identity user object
+    identity.user = current_user
+
+    # Add the UserNeed to the identity
+    if hasattr(current_user, 'id'):
+        identity.provides.add(UserNeed(current_user.id))
+
+    # Assuming the User model has a list of roles, update the
+    # identity with the roles that the user provides
+    if hasattr(current_user, 'roles'):
+        for role in current_user.roles:
+            identity.provides.add(RoleNeed(role))
--- nokkhum/web/views/admin/__init__.py
+++ nokkhum/web/views/admin/__init__.py
@@ -7,6 +7,5 @@ module = Blueprint('admin', __name__, url_prefix='/admin')
 
 
 @module.route('/')
-@acl.allows.requires(acl.is_admin)
 def index():
     return 'admin'
