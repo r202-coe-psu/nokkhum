87bd9314cfa063504fd4e8d76bbb19490261adeb
Somboon Changkhom <mypspgood@gmail.com>
2021-06-09 13:39:26 +0700

    update share date dir for each processor

--- nokkhum/web/views/dashboard.py
+++ nokkhum/web/views/dashboard.py
@@ -9,36 +9,50 @@ module = Blueprint("dashboard", __name__, url_prefix="/dashboard")
 subviews = []
 
 
-def index_admin(search_keyword):
-    projects = models.Project.objects(
-        status="active", name__icontains=search_keyword
-    ).order_by("-id")
-    # for project in projects:
-    #     my_projects.append(project)
-    return render_template("/dashboard/index.html", projects=projects)
+# def index_admin(search_keyword):
+#     projects = models.Project.objects(
+#         status="active", name__icontains=search_keyword
+#     ).order_by("-id")
+#     # for project in projects:
+#     #     my_projects.append(project)
+#     return render_template("/dashboard/index.html", projects=projects)
 
 
-def index_user(search_keyword):
-    projects = models.Project.objects(
-        Q(name__icontains=search_keyword)
-        & Q(status="active")
-        & (
-            Q(owner=current_user._get_current_object())
-            | Q(users__icontains=current_user._get_current_object())
-            | Q(assistant__icontains=current_user._get_current_object())
-        )
-    ).order_by("-id")
-    return render_template("/dashboard/index.html", projects=projects)
+# def index_user(search_keyword):
+#     projects = models.Project.objects(
+#         Q(name__icontains=search_keyword)
+#         & Q(status="active")
+#         & (
+#             Q(owner=current_user._get_current_object())
+#             | Q(users__icontains=current_user._get_current_object())
+#             | Q(assistant__icontains=current_user._get_current_object())
+#         )
+#     ).order_by("-id")
+#     return render_template("/dashboard/index.html", projects=projects)
 
 
 @module.route("/")
 @login_required
 def index():
     project_search = request.args.get("project_search", "")
-    # print(project_search)
-    # if project_search:
-    #     return index_search(project_search)
-    # user = current_user._get_current_object()
-    if "admin" in current_user._get_current_object().roles:
-        return index_admin(project_search)
-    return index_user(project_search)
+    storage_share_list = models.StorageShare.objects(
+        psu_passport_username=current_user._get_current_object().username,
+        start_date__lte=datetime.date.today(),
+        expire_date__gte=datetime.date.today(),
+    )
+    projects = models.Project.objects(
+        Q(name__icontains=project_search)
+        & Q(status="active")
+        & (
+            Q(owner=current_user._get_current_object())
+            | Q(users__icontains=current_user._get_current_object())
+            | Q(assistant__icontains=current_user._get_current_object())
+            | Q(security_guard__icontains=current_user._get_current_object())
+        )
+    ).order_by("-id")
+
+    return render_template(
+        "/dashboard/index.html",
+        projects=projects,
+        storage_share_list=storage_share_list,
+    )
